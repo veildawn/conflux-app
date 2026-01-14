3f3eae4f402f79c32a0c521d56b8e91f33a6f33c
Eric <40253459+veildawn@users.noreply.github.com>
2026-01-14 13:34:55 +0800

    更新配置

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// https://vite.dev/config/
+// https://vite.dev/config
 export default defineConfig({
   plugins: [react()],
 
